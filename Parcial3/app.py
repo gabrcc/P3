d5a680c0f9417493f1d6dd4ffad995eedd715868
gabrcc <cuellar.caero.gabriela@gmail.com>
2023-06-29 00:59:06 -0400

    finalReports

--- Parcial3/app.py
+++ Parcial3/app.py
@@ -21,55 +21,54 @@ def index():
         return redirect('/dash')
     return render_template('index.html')
 
-# @app.route('/dash', methods=['GET', 'POST'])
-# def dash():
-#     global image_counter
-#     if request.method == 'POST':
-#         variable = request.form['variable']
-#         data = pd.read_csv('static/test.csv')
-#         columns = data.columns.tolist()
-#         # size = len(data[variable])
-#         # plt.bar(range(size), data[variable])
-#         # imagepathbar = os.path.join('static', 'bar' + '.png')
-#         # plt.savefig(imagepathbar)
-#         plt.plot(data[variable])
-#         image_counter += 1 
-#         imagepathplot = os.path.join('static', f'plot{image_counter}' + '.png')
-#         plt.savefig(imagepathplot)
-#         plt.close()
-#         return render_template('dash.html', imageplot = imagepathplot, columns=columns)
-#     return render_template('dash.html')
 
 @app.route('/dash', methods=['GET', 'POST'])
 def dash():
     global image_counter
     if request.method == 'POST':
         variable = request.form['variable']
+        variable2 = request.form['variable2']
         data = pd.read_csv('static/test.csv')
         columns = data.columns.tolist()
         chart_type = request.form['chart_type']
+        size = len(data[variable])
 
         if chart_type == 'line':
             plt.plot(data[variable])
             plt.title(f'{variable} - Líneal')
         elif chart_type == 'bar':
-            size = len(data[variable])
             plt.bar(range(size), data[variable])
             plt.title(f'{variable} - Barra')
         elif chart_type == 'histogram':
             plt.hist(data[variable])
             plt.title(f'{variable} - Histograma')
+        elif chart_type == 'scatter':
+            plt.scatter(data[variable], data[variable2])
+            plt.title(f'{variable} - Scatter')
+        elif chart_type == 'pie':
+            count = data[variable].value_counts()
+            plt.pie(count, labels=count.index)
+            plt.title(f'{variable} - Pie')
+        elif chart_type == 'boxplot':
+            plt.boxplot(data[variable])
+            plt.title(f'{variable} - BoxPlot')
+        elif chart_type == 'violinplot':
+            plt.violinplot(data[variable])
+            plt.title(f'{variable} - ViolinPlot')
+        elif chart_type == 'area':
+            plt.fill_between(range(size), data[variable])
+            plt.title(f'{variable} - Area')
 
 
         image_counter += 1 
         imagepathplot = os.path.join('static', f'plot{image_counter}' + '.png')
         plt.savefig(imagepathplot)
         plt.close()
-        return render_template('dash.html', imageplot = imagepathplot, columns=columns,chart_types=['line', 'bar', 'histogram'])
+        return render_template('dash.html', imageplot = imagepathplot, columns=columns,chart_types=['line', 'bar', 'histogram','scatter','pie','boxplot','violinplot','area'])
     
     data = pd.read_csv('static/test.csv')
     columns = data.columns.tolist()
-    return render_template('dash.html', columns=columns , chart_types=['line', 'bar', 'histogram'])
+    return render_template('dash.html', columns=columns , chart_types=['line', 'bar', 'histogram','scatter','pie','boxplot','violinplot','area'])
 
 
 
